5d0c12a7353f830f88a9e1c6b4878772af19e0a5
Norua88 <31585005+Norua88@users.noreply.github.com>
2018-01-06 01:48:54 +0400

    change
    small change

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+           
+        
+        <title></title>
+
+        </head>
+    <body class="is-loading">
+        
+      <div id="chibi_spartaco"></div>
+        
+        
+
+
+
+        
+        
+        <script type="text/javascript" src="chibi.js"></script>
+        
+        <!-- for Chibi Canvas -->
+            <script type="text/javascript">
+                prepareCanvas(document.getElementById("chibi_spartaco"), 280, 420);
+            </script>
+        
+       
+    </body>
+</html>
